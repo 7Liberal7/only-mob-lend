9a541faea3020e998f13989878ce86947710d79c
7Liberal7 <89695467+7Liberal7@users.noreply.github.com>
2022-04-09 00:18:04 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function () {
+  $('.how-study__slider').slick({
+    arrows: true,
+    dots: false,
+    swipe: false,
+    slidesToShow: 1,
+    prevArrow: '<button class="slick-prev slick__btn" type="button"><img class="how-study__arrow how-study__left" src="images/arrow-left.png"></button>',
+    nextArrow: '<button class="slick-next slick__btn" type="button"><img class="how-study__arrow how-study__right" src="images/arrow-right.png"></button>'
+  });
+});
+
+$(function () {
+  $('.lesson__slider').slick({
+    arrows: true,
+    dots: false,
+    swipe: false,
+    slidesToShow: 1,
+    prevArrow: '<button class="slick-prev slick__btn" type="button"><img class="how-study__arrow how-study__left" src="images/arrow-left.png"></button>',
+    nextArrow: '<button class="slick-next slick__btn" type="button"><img class="how-study__arrow how-study__right" src="images/arrow-right.png"></button>'
+  });
+});
